b90aa653a4a3cd11436f2248b299b4dd9e426728
kevinmenden <kevin.menden@t-online.de>
2020-02-06 21:26:36 +0100

    working cartpole version

--- testing.py
+++ testing.py
@@ -9,9 +9,7 @@ import torchvision.transforms as T
 import torch.optim as optim
 import gym
 import random
-from collections import namedtuple
-from torch.nn.init import kaiming_uniform_
-import numpy as np
+import matplotlib.pyplot as plt
 
 
 class DQN(nn.Module):
@@ -20,22 +18,14 @@ class DQN(nn.Module):
     Model Parameters like in Mnih et al., 2015
     """
 
-    def __init__(self, n_actions=4, feature_size=3136):
+    def __init__(self, n_actions=2):
         super(DQN, self).__init__()
         self.n_actions = n_actions
-        self.feature_size = feature_size
 
-        self.conv1 = nn.Conv2d(2, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(self.feature_size, 512)
-        self.fc2 = nn.Linear(512, self.n_actions)
+        self.fc1 = nn.Linear(4, 256)
+        self.fc2 = nn.Linear(256, self.n_actions)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -108,16 +98,6 @@ def choose_action(state, epsilon, q_network, n_actions):
             return torch.argmax(q_network.forward(state))
 
 
-def transform_frame(frame):
-    """
-    Transform a frame to tensor of shape (batch_size, 1, 84, 84)
-    :param frame:
-    :return:
-    """
-    frame = T.Compose([T.ToPILImage(), T.Resize((84, 84)), T.Grayscale(), T.ToTensor()])(frame)
-    frame = frame / 255
-    return frame
-
 def game_step(env, action, n_steps=4):
     """
     Play one step of the game
@@ -126,37 +106,26 @@ def game_step(env, action, n_steps=4):
     :n_steps: number of frames to play
     :return:
     """
-    frames = []
-    reward = 0
+    state, reward, done, _  = env.step(action)
     last_state = False
-    for i in range(n_steps):
-        s, r, done, _ = env.step(action)
-        frame = env.render(mode='rgb_array').transpose((2, 0, 1))
-        if done:
-            last_state = True
-            reward += r
-        else:
-            frames.append(transform_frame(frame))
-            reward += r
-    if last_state:
-        state = None
-    else:
-        state = torch.stack(frames, 0)
-        state = state.transpose(0, 1)
+    if done:
+        reward = 0
+        last_state = True
 
     return (state, reward, last_state)
 
+
 def extract_batch(batch, slot):
     if slot == 'action':
-        return torch.tensor([x.action for x in batch])
+        return torch.tensor([int(x.action) for x in batch])
     elif slot == 'state':
-        return torch.cat([x.state for x in batch])
+        return torch.cat([torch.Tensor(x.state).unsqueeze(0) for x in batch])
     elif slot == 'reward':
         return torch.tensor([x.reward for x in batch])
     elif slot == 'next_state':
         # return only non-final next states
         next_states = [x.next_state for x in batch]
-        non_final_next_states = torch.cat([s for s in next_states if s is not None])
+        non_final_next_states = torch.cat([torch.Tensor(s).unsqueeze(0) for s in next_states if s is not None])
         return non_final_next_states
     elif slot == 'non_final':
         return torch.tensor([x.non_final for x in batch])
@@ -179,14 +148,16 @@ def training_step(policy, target, memory, optimizer, criterion, batch_size=32, g
     batch = memory.sample(batch_size)
 
     # extract state, action, reward, next_state
-    action_batch = extract_batch(batch, 'action').to(device).unsqueeze(1)
     state_batch = extract_batch(batch, 'state').to(device)
     reward_batch = extract_batch(batch, 'reward').to(device)
     next_state_batch = extract_batch(batch, 'next_state').to(device)
+    action_batch = extract_batch(batch, 'action').to(device)
     non_final_mask = extract_batch(batch, 'non_final')
+
 
     # q-values
     q_value = policy(state_batch)
+    action_batch = action_batch.unsqueeze(1)
     q_value = q_value.gather(1, action_batch).squeeze(1)
 
     # q-values for next state
@@ -212,17 +183,18 @@ Main Section
 """
 
 # === PARAMETERS ===#
-batch_size = 8
-max_screens = 1400000
-memory_capacity = 30000
+batch_size = 64
+memory_capacity = 10000
 memory_init_size = 100
-gamma = 0.99
+gamma = 0.8
 target_update = 100
 epsilon_start = 1
-epsilon_end = 0.01
-epsilon_steps = 2000
-n_steps = 2
-lr = 0.000625
+epsilon_end = 0.05
+epsilon_steps = 10000
+n_steps = 1
+lr = 0.001
+eval_freq = 20
+episodes = 4000
 
 # Create Breakout environment
 env = gym.make('CartPole-v0')
@@ -246,19 +218,17 @@ memory = ReplayBuffer(memory_capacity)
 
 epsilon = epsilon_start
 epsilon_delta = (epsilon_start - epsilon_end) / epsilon_steps
+rewards = []
 
-screens = 0
-
-while screens < max_screens:
+total_reward = 0
+for ep in range(episodes):
 
     # reset env for new episode
     env.reset()
     # get initial state
     state, _, _ = game_step(env, env.action_space.sample(), n_steps=n_steps)
     complete_reward = 0
-
     for t in count():
-        screens += 1
 
         # adjust epsilon
         epsilon = epsilon - epsilon_delta
@@ -266,11 +236,12 @@ while screens < max_screens:
             epsilon = epsilon_end
 
         # choose action based on current state
-        action = choose_action(state.cuda(), epsilon, policy, n_actions).detach().cpu().numpy()
+        action = choose_action(torch.Tensor(state).cuda(), epsilon, policy, n_actions).detach().cpu().numpy()
 
         # make one step
         next_state, reward, done = game_step(env, action, n_steps=n_steps)
         complete_reward += reward
+        total_reward += reward
         reward = torch.tensor([reward], dtype=torch.float32)
 
         # save the current experience
@@ -279,16 +250,21 @@ while screens < max_screens:
         # update state variable
         state = next_state
 
-        if screens > memory_init_size:
-            # Perform one step of training on the policy network
-            training_step(policy, target, memory, optimizer, criterion=criterion, batch_size=batch_size, gamma=gamma)
+        training_step(policy, target, memory, optimizer, criterion=criterion, batch_size=batch_size, gamma=gamma)
 
-            # Update the target network after 10000 frames seen
-            if screens % target_update == 0:
-                target.load_state_dict(policy.state_dict())
-                print("update target")
+        if ep % target_update == 0:
+            target.load_state_dict(policy.state_dict())
 
-        if done:
+        if done and ep % eval_freq == 0:
             mem_len = len(memory.memory)
-            print(f"Reward: {complete_reward}, Epsilon: {epsilon}, Memory: {mem_len}, Frames: {screens}")
+            avg_reward = total_reward / eval_freq
+            rewards.append(avg_reward)
+            print(f"Reward: {avg_reward}, Epsilon: {epsilon}, Memory: {mem_len}, Episodes: {ep}")
+            total_reward = 0
             break
+        if done:
+            break
+
+
+plt.plot(rewards)
+plt.show()
